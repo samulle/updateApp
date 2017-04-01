7cdc5283fbb16deb76ed6aa8a73f709d6940d771
shiyong <shiyong@shiyongs-MacBook-Pro.local>
2017-04-01 10:46:30 +0800

    add some new content

--- www/UpdateApp.js
+++ www/UpdateApp.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
+exports.update = function(arg0, success, error) {
     exec(success, error, "UpdateApp", "", [arg0]);
 };
